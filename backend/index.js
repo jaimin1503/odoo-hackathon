d0558c768708e222d7139d711fcb8554da38bcac
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-06-29 19:48:13 +0530

    deploy settings

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://task-me-bay.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -24,7 +24,7 @@ app.use(
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "https://task-me-bay.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
